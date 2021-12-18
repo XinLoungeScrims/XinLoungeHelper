a2716f3d8cadfaa63a31d14bddaa35099aeff551
Metacinnabar <45357714+Metacinnabar@users.noreply.github.com>
2021-12-19 08:43:36 +1100

    Fix environment keys

--- XinLoungeHelper/Program.cs
+++ XinLoungeHelper/Program.cs
@@ -3,9 +3,15 @@ using Tomat.Framework.Core.Bot;
 using XinLoungeHelper;
 
 // load our environment variables containing our token
-DotNetEnv.Env.Load();
-//Console.WriteLine("token: " + Environment.GetEnvironmentVariable("TOKEN"));
+DotNetEnv.Env.TraversePath().Load();
+
+// check if token actually loaded
+string? token = Environment.GetEnvironmentVariable("TOKEN");
+if (string.IsNullOrEmpty(token))
+{
+    throw new Exception("Token was not loaded or was found empty.");
+}
 
 // initialise and start the bot with the token found in .env
-using DiscordBot discordBot = new XinLoungeHelperBot("");
+using DiscordBot discordBot = new XinLoungeHelperBot(token);
 await discordBot.StartBot();
